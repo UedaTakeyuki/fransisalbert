b0aa774ed2b41d1d4aa10a8cc5ab719a721bbb44
Takeyuki UEDA <ueda@latelierdueda.com>
2021-10-18 20:18:40 +0900

    use ReadAll

--- fransisalbert.go
+++ fransisalbert.go
@@ -5,8 +5,9 @@
 
 package fransisalbert
 
-
 import (
+	"io"
+	"io/ioutil"
 	"log"
 
 	"github.com/gin-gonic/gin"
@@ -16,10 +17,15 @@ import (
  * getRequestBody
  */
 func GetRequestBody(c *gin.Context) (body []byte) {
-	buf := make([]byte, 4096)
-	n, _ := c.Request.Body.Read(buf)
-	//		body := string(buf[0:n])
-	body = buf[0:n]
-	log.Println("body = ", string(body))
+	//	buf := make([]byte, 4096)
+	//	n, _ := c.Request.Body.Read(buf)
+	//	body = buf[0:n]
+
+	// https://stackoverflow.com/a/38874756/11073131
+	body, err := ioutil.ReadAll(io.LimitReader(c.Request.Body, 65536))
+	if err != nil {
+		log.Println(err)
+	}
+	//	log.Println("body = ", string(body))
 	return
 }
